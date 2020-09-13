92c3d1811599ec259b257b63909edce297aca5de
Ionut V <67919936+ionutMan07@users.noreply.github.com>
2020-09-13 12:00:21 +0300

    done

--- codewars.js
+++ codewars.js
@@ -31,3 +31,29 @@ function getCount(str) {
   return vowelsCount;
 }
 getCount("adapoirewiernlkdhf");
+
+/* Define a method hello that returns "Hello, Name!" to a given name, or says Hello, World! if name is not given (or passed as an empty String).
+
+Assuming that name is a String and it checks for user typos to return a name with a first capital letter (Xxxx).
+*/
+
+function hello(name) {
+  if (name === undefined){
+    return 'Hello, World!'
+  }else if (name === ''){
+    return 'Hello, World!'
+  }else
+  return 'Hello, ' + name[0].toUpperCase() + name.substring(1).toLowerCase() + '!';
+}
+
+function hello(name) {
+  return name ? 'Hello, ' + name.slice(0, 1).toUpperCase() + name.slice(1).toLowerCase() + '!' : 'Hello, World!';
+}
+
+function hello(name) {
+  return name ? 'Hello, ' + name[0].toUpperCase() + name.substring(1).toLowerCase() + '!' : 'Hello, World!';
+}
+
+function hello(name) {
+  return `Hello, ${name ? name[0].toUpperCase() + name.slice(1).toLowerCase() : "World"}!`
+}
